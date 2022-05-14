1fbf45151c6e355f1a2a8a583a16b206a47f1664
kosukeGun <ko.iwai09210921@icloud.com>
2022-05-14 19:43:32 +0900

    【追加】phpファイルの試作品

---
+++ php-date1.php
@@ -0,0 +1,12 @@
+<?php 
+  class changeDate{
+    private $dayNames=array("月曜日","火曜日","水曜日","木曜日","金曜日","土曜日","日曜日");
+    
+    public function getDateNames(){
+      return this->dayNames;
+  }
+  
+    
+  
+  
+?>
